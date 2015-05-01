b6d41d43bb595bcfbf0ba35642eee59de58588f5
Holger Thies <info@holgerthies.com>
2015-05-01 18:04:28 +0200

    lineplot area under curve can be filled

--- lineplot.js
+++ lineplot.js
@@ -27,6 +27,11 @@ simplePlot.linePlot = function(){
 		    return yRange(d.y);
 		  })
 		  .interpolate('linear');
+    // used to fill the area below the curve
+		var	area = d3.svg.area()
+	    .x(function(d) { return xRange(d.x); })
+	    .y0(height-20)
+	    .y1(function(d) { return yRange(d.y); });
     // function to filter only those values that are in the xrange
     var filter = function(arr){
       var result=[];
@@ -40,12 +45,27 @@ simplePlot.linePlot = function(){
       if(hidden.indexOf(i+1) == -1)
       {
         var linedata = filter(data[i]);
+        // draw line
         selection.append('svg:path')
           .attr('d', lineFunc(linedata))
           .attr('stroke', 'black')
           .attr('class', 'line-'+(i+1))
           .attr('stroke-width', 2)
           .attr("fill", "none");
+        // area below the curve
+        selection.append('svg:path')
+          .attr('d', area(linedata))
+          .attr('class', 'area-'+(i+1));
+        // draw data points
+        for(j = 0; j <linedata.length; j++){
+          selection.append('circle')
+            .attr('r', 3)
+            .attr('cx', xRange(linedata[j].x))
+            .attr('cy', yRange(linedata[j].y))
+            .attr('class', 'dot-'+(i+1))
+            .append('title')
+            .text(linedata[j].y);
+        }
       }
     }
   }
